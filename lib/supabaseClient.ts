7ecb53c7cb2c2d4d735c0928bd566eec4fe472d2
SeraKah-1 <farreladitya38@gmail.com>
2026-02-11 02:51:00 +0000

    Update fix kode dari mikirs

--- lib/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-
-// We create a function to get the client because the URL/Key can change dynamically 
-// based on user settings in local storage.
-export const getSupabaseClient = (url: string, key: string): SupabaseClient => {
-  return createClient(url, key);
-};
